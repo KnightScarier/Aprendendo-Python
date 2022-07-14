4eb5c7cbac688a05ecb0a90aba22b317485e8f10
Gabriel Fernandes <69177910+KnightScarier@users.noreply.github.com>
2022-07-14 02:53:26 -0300

    Update Aula 1 - Tipos de Comentarios.py

--- Aula 1 - Tipos de Comentarios.py
+++ Aula 1 - Tipos de Comentarios.py
@@ -15,19 +15,19 @@ Que retorna se o valor é da classe: 'Str, Bool, Int, ou Float'
 
 Inteiros -> São Números com o valor Inteiro.
 -> (...-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9...)
-Ex: print (0),(1),(2),(...)
+Ex: print(3000)
 
 Floats -> São Números com o valor 'flutuante', são números com casas decimais.
 -> (...0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0;1.1;...)
-Ex: print (3.14)
+Ex: print(3.14)
 
-Strings -> Todo tipo de dado que está entre 'aspas', assim como números.
+Strings -> Todo tipo de dado que está entre 'aspas', assim como números, retornará qualquer parâmetro em texto
 -> ('Olá, Mundo!')
-Ex: print ("Olá,Mundo")
+Ex: print("Olá,Mundo")
 
 Boolean -> Todos os valores que são: 'True or False', podendo também ser representado como: 0 = False 1 = True
 -> ('False')
-Ex: print (False) ou print (True)
+Ex: print(False) ou print(True)
 """
 
 print("Hello, World!")
