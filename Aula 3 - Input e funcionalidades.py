b1397ef24cdea1580800fdc64501ed271f3820b2
Gabriel Fernandes <69177910+KnightScarier@users.noreply.github.com>
2022-07-14 03:04:39 -0300

    Update Aula 3 - Input e funcionalidades.py

--- Aula 3 - Input e funcionalidades.py
+++ Aula 3 - Input e funcionalidades.py
@@ -38,9 +38,8 @@ print(f"Meu nome é {nome}\nMinha idade é {idade}\nMeu peso é {peso}kg")
 # Por que utilizar dessa maneira? E se não especificarmos as variáveis?
 """
 
-"""
 ##Obs: Para que não haja erros de formatação, como no exemplo abaixo:
-"""
+
 
 # Observe que o 'input' não está especificando qual a variável (str,int,bool ou float) do dado inserido
 # para testar o código, basta retirar as aspas triplas
@@ -59,16 +58,27 @@ print(f"Meu peso é: {peso}{type(peso)}")
 """
 
 # Diferença entre um dado String e um dado Inteiro
-# para testar o código, basta retirar as aspas triplas
 
 # Perceba que no parâmetro 'string' há a presença de aspas pois o dado de 'string' retornará um texto
-string = "10"  # parâmetro de str *aplicado*
+
+string = "10"  # parâmetro de str
 
 # Perceba que na variante 'inteiro' não há a presença de aspas, pois o dado de 'inteiro' retornará um número
-inteiro = 10  # parâmetro de int *aplicado*
+inteiro = 10  # parâmetro de int
 
 # Exemplos da diferença entre essas variáveis com códigos
 
 # Perceba que neste código, a soma se dá porque 'inteiro' é equivalente a 10, aplicado no parâmetro anterior
 print(inteiro + 10)  # por isso, este código retornará 10+10 = 20
 print(string + "10")  # por isso, este código retornará "10"+"10" = 1010, pois considera ambos como texto.
+
+# para melhor visualização: da diferença que há entre eles
+# para testar o código, basta retirar as aspas triplas
+
+"""
+string = "10"
+inteiro = 10
+
+print(type(string))
+print(type(inteiro))
+"""
