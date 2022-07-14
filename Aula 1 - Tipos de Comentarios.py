31153ba5e476291b91af7457d5c5e0455a21afc6
Gabriel Fernandes <69177910+KnightScarier@users.noreply.github.com>
2022-07-14 03:20:12 -0300

    Update Aula 1 - Tipos de Comentarios.py

--- Aula 1 - Tipos de Comentarios.py
+++ Aula 1 - Tipos de Comentarios.py
@@ -8,26 +8,34 @@ Ex2: #exemplo - Comentário com '#'
 """
 
 # <-- Tipos de Dados -->
+
+
+# ##Obs: para testar cada uma das classes desses modos podemos usar: print(type(...)).
+
+# Que retorna se o valor é da classe: 'Str, Bool, Int, ou Float' 
+
+# Inteiros -> São Números com o valor Inteiro.
+"""
+ -> (...-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9...)
+ Ex: print(3000)
+ """
+
+# Floats -> São Números com o valor 'flutuante', são números com casas decimais.
+"""
+# -> (...0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0;1.1;...)
+# Ex: print(3.14)
 """
 
-##Obs: para testar cada uma das classes desses modos podemos usar: print(type(...)).
-Que retorna se o valor é da classe: 'Str, Bool, Int, ou Float' 
+# Strings -> Todo tipo de dado que está entre 'aspas', assim como números, retornará qualquer parâmetro em texto
+"""
+# -> ('Olá, Mundo!')
+# Ex: print("Olá,Mundo")
+"""
 
-Inteiros -> São Números com o valor Inteiro.
--> (...-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9...)
-Ex: print(3000)
-
-Floats -> São Números com o valor 'flutuante', são números com casas decimais.
--> (...0.1;0.2;0.3;0.4;0.5;0.6;0.7;0.8;0.9;1.0;1.1;...)
-Ex: print(3.14)
-
-Strings -> Todo tipo de dado que está entre 'aspas', assim como números, retornará qualquer parâmetro em texto
--> ('Olá, Mundo!')
-Ex: print("Olá,Mundo")
-
-Boolean -> Todos os valores que são: 'True or False', podendo também ser representado como: 0 = False 1 = True
--> ('False')
-Ex: print(False) ou print(True)
+# Boolean -> Todos os valores que são: 'True or False', podendo também ser representado como: 0 = False 1 = True
+"""
+# -> ('False')
+# Ex: print(False) ou print(True)
 """
 
 print("Hello, World!")
